eaf29a9e40c293fdb678997b40d48bcc35a662b1
floris <floris-xlx@65.108.104.231>
2025-02-15 19:06:47 +0100

    Enhance server performance and caching with async improvements
    - Add num_cpus for dynamic worker configuration
    - Implement async-safe cache using Moka
    - Refactor proxy request handling with improved caching strategy
    - Optimize client configuration and tracing
    - Replace Mutex with async-friendly cache implementation

--- src/main.rs
+++ src/main.rs
@@ -5,130 +5,142 @@ use actix_web::get;
 use actix_web::http::{header, StatusCode};
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
-use reqwest::Method;
-use serde_json::{json, Value};
-
 use moka::future::Cache;
-use reqwest::Client;
+use reqwest::{Client, Method};
+use serde_json::{json, Value};
 use std::env::var;
 use std::io::Result;
 use std::sync::Arc;
 use std::time::Duration;
-use tokio::sync::Mutex;
+use tracing::info;
 use tracing_subscriber::EnvFilter;
 use web::Data;
 
-pub type SharedCache = Arc<Mutex<Cache<String, Value>>>;
+pub struct AppState {
+    cache: Arc<Cache<String, Value>>, // Removed Mutex for async-safe cache
+    client: Client,
+}
 
 const TARGET_BASE_URL: &str = "https://db.xylex.cloud";
 
 #[get("/")]
 async fn ping() -> impl Responder {
-    println!("Received ping request");
+    info!("Received ping request");
     HttpResponse::Ok().json(json!({"message": "pong"}))
 }
 
-async fn proxy_request(req: HttpRequest, body: web::Bytes) -> impl Responder {
-    println!("Proxy request received: {:?}", req.full_url().path());
-    let client: Client = Client::new();
-    let full_url: reqwest::Url = req.full_url();
-    let full_url_path: &str = full_url.path();
-
-    // Remove "/rest/v1" from the path if it exists
-    let path: String = full_url_path.replacen("/rest/v1", "", 1);
-    println!("Processed path: {}", path);
+async fn proxy_request(
+    req: HttpRequest,
+    body: web::Bytes,
+    app_state: Data<AppState>,
+) -> impl Responder {
+    let client = &app_state.client;
+    let cache = &app_state.cache;
+    let full_url = req.full_url();
+    let full_url_path = full_url.path();
+    let path = full_url_path.replacen("/rest/v1", "", 1);
+    info!("path: {:#?}", path);
 
     let target_url: String = format!("{}{}", TARGET_BASE_URL, path);
-    println!("Target URL: {}", target_url);
+    info!("target_url {:#?}", target_url);
 
-    // Convert actix_web::http::Method to reqwest::Method
-    let reqwest_method = match req.method() {
-        &actix_web::http::Method::GET => reqwest::Method::GET,
-        &actix_web::http::Method::POST => reqwest::Method::POST,
-        &actix_web::http::Method::PUT => reqwest::Method::PUT,
-        &actix_web::http::Method::DELETE => reqwest::Method::DELETE,
-        &actix_web::http::Method::HEAD => reqwest::Method::HEAD,
-        &actix_web::http::Method::OPTIONS => reqwest::Method::OPTIONS,
-        &actix_web::http::Method::CONNECT => reqwest::Method::CONNECT,
-        &actix_web::http::Method::PATCH => reqwest::Method::PATCH,
-        &actix_web::http::Method::TRACE => reqwest::Method::TRACE,
-        _ => reqwest::Method::GET, // Default to GET if method is unknown
-    };
+    let jwt_token = req
+        .headers()
+        .get(header::AUTHORIZATION)
+        .and_then(|value| value.to_str().ok())
+        .map(|value| value.to_string())
+        .unwrap_or_default();
 
-    let mut client_req: reqwest::RequestBuilder = client.request(reqwest_method, &target_url);
+    let cache_control_header: Option<header::HeaderValue> =
+        req.headers().get(header::CACHE_CONTROL).cloned();
 
-    // Copy headers
-    for (key, value) in req.headers().iter() {
-        if key != header::HOST {
-            client_req = client_req.header(
-                reqwest::header::HeaderName::from_bytes(key.as_ref()).unwrap(),
-                reqwest::header::HeaderValue::from_bytes(value.as_bytes()).unwrap(),
-            );
+    let cachekey = format!("{}-{}-{}", req.method(), full_url, jwt_token)
+        .replace('*', "_xXx_")
+        .replace(' ', "_")
+        .replace(':', "-")
+        .replace('/', "_");
+
+    if cache_control_header
+        .as_ref()
+        .map_or(true, |h| h != "no-cache")
+    {
+        if let Some(cached_response) = cache.get(&cachekey).await {
+            return HttpResponse::Ok().json(cached_response);
         }
     }
-    println!("Headers copied to client request");
 
-    // Send request
-    let client_response = client_req.body(body).send().await;
-    println!("Request sent to target URL");
-
-    match client_response {
+    let reqwest_method = match *req.method() {
+        actix_web::http::Method::GET => Method::GET,
+        actix_web::http::Method::POST => Method::POST,
+        actix_web::http::Method::PUT => Method::PUT,
+        actix_web::http::Method::DELETE => Method::DELETE,
+        actix_web::http::Method::PATCH => Method::PATCH,
+        _ => Method::GET,
+    };
+    let mut client_req = client.request(reqwest_method, &target_url);
+    for (key, value) in req.headers().iter() {
+        if key != header::HOST {
+            let reqwest_key = reqwest::header::HeaderName::from_bytes(key.as_ref()).unwrap();
+            let reqwest_value = reqwest::header::HeaderValue::from_bytes(value.as_bytes()).unwrap();
+            client_req = client_req.header(reqwest_key, reqwest_value);
+        }
+    }
+    match client_req.body(body).send().await {
         Ok(res) => {
-            println!("Received response from target URL with status: {}", res.status());
-            let mut client_resp = HttpResponse::build(
-                actix_web::http::StatusCode::from_u16(res.status().as_u16()).unwrap(),
-            );
-            for (key, value) in res.headers().iter() {
-                if key != &reqwest::header::CONTENT_ENCODING
-                    && key != &reqwest::header::CONTENT_LENGTH
-                    && key != &reqwest::header::TRANSFER_ENCODING
-                    && key != &reqwest::header::CONNECTION
+            let status_code = StatusCode::from_u16(res.status().as_u16()).unwrap();
+            let headers = res.headers().clone();
+            let body_bytes = res.bytes().await.unwrap_or_default();
+            let json_body: Value = serde_json::from_slice(&body_bytes).unwrap_or(Value::Null);
+            cache.insert(cachekey, json_body.clone()).await;
+            let mut response = HttpResponse::build(status_code);
+
+            for (key, value) in headers.iter() {
+                if ![
+                    reqwest::header::CONTENT_ENCODING,
+                    reqwest::header::CONTENT_LENGTH,
+                    reqwest::header::TRANSFER_ENCODING,
+                    reqwest::header::CONNECTION,
+                ]
+                .contains(key)
                 {
-                    if let (Ok(actix_key), Ok(actix_value)) = (
-                        actix_web::http::header::HeaderName::try_from(key.as_str()),
-                        actix_web::http::header::HeaderValue::from_str(
-                            value.to_str().unwrap_or(""),
-                        ),
-                    ) {
-                        client_resp.append_header((actix_key, actix_value));
-                    }
+                    let actix_key =
+                        actix_web::http::header::HeaderName::from_bytes(key.as_str().as_bytes())
+                            .unwrap();
+                    let actix_value =
+                        actix_web::http::header::HeaderValue::from_bytes(value.as_bytes()).unwrap();
+                    response.append_header((actix_key, actix_value));
                 }
             }
-            let body = res.bytes().await.unwrap_or_default();
-            client_resp.body(body)
+            response.body(body_bytes)
         }
-        Err(e) => {
-            println!("Error occurred while sending request: {:?}", e);
-            HttpResponse::InternalServerError().finish()
-        }
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    println!("Starting server...");
-    init_tracing();
     dotenv().ok();
+    init_tracing();
     let port: u16 = var("XLX_ATHENA_PORT")
-        .unwrap_or("4052".to_string())
+        .unwrap_or_else(|_| "4052".to_string())
         .parse()
         .unwrap_or(4052);
-    println!("Server will run on port: {}", port);
-
-    let cache: SharedCache = Arc::new(Mutex::new(
+    let cache = Arc::new(
         Cache::builder()
             .time_to_live(Duration::from_secs(60))
             .build(),
-    ));
-    println!("Cache initialized with TTL of 60 seconds");
+    );
+    let client = Client::builder()
+        .pool_idle_timeout(Duration::from_secs(90))
+        .build()
+        .unwrap();
+    let app_state = Data::new(AppState { cache, client });
 
     HttpServer::new(move || {
-        let cors: Cors = Cors::default()
+        let cors = Cors::default()
             .allow_any_origin()
             .allow_any_method()
             .allow_any_header();
-        println!("CORS configured to allow any origin, method, and header");
-
         App::new()
             .wrap(cors)
             .wrap_fn(|req, srv| {
@@ -140,20 +152,18 @@ async fn main() -> Result<()> {
                     Ok(res)
                 }
             })
-            .app_data(Data::new(cache.clone()))
+            .app_data(app_state.clone())
             .service(ping)
             .default_service(web::route().to(proxy_request))
     })
-    .workers(16)
+    .workers(num_cpus::get() * 2)
+    .keep_alive(Duration::from_secs(75))
     .bind(("0.0.0.0", port))?
     .run()
     .await
 }
 
 fn init_tracing() {
-    let filter: EnvFilter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     tracing_subscriber::fmt().with_env_filter(filter).init();
-    println!("Tracing initialized with environment filter");
 }
